8d0c6443f8ffbfc5424f72833b89e28618f64412
Laura Imrie <laura@utnsl.com>
2012-08-23 19:22:43 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utnsl::Application.config.secret_token = 'e8a31906cd8bc60030e36e4b17937d9c5b49f4f22939c8ff6d02f7c245865bef5bbe6ae26332ecd4d6ed186a2efa65670395efa3ca8f358df97d9b1219a27fa8'
